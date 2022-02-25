06c38a54d6832481ee8137a17ccc97cb97b303d9
dmitry.petuhov <dmitry.petuhov@profitclicks.ru>
2022-02-25 15:56:11 +0300

    init: init project

---
+++ javaScript/advancedExpressions.js
@@ -0,0 +1,25 @@
+// * Advanced Expressions
+
+// - Hoisting
+hoistingFunc(); // hoisting
+
+function hoistingFunc() {
+  console.log('hoisting');
+}
+
+console.log(a); // undefined
+var a = 10;
+console.log(a); // 10
+
+// - Temporal dead zone - нельзя обращатсья к переменным неподверженным Hoisting выше по коду
+console.log(b); // undefined
+var b = 20;
+console.log(b); // undefined
+
+console.log(c); // ReferenceError: Cannot access 'c' before initialization
+console.log(d); // ReferenceError: Cannot access 'd' before initialization
+
+// end temporal zone for c
+const c = 1;
+// end temporal zone for d
+let d = 2;
---
+++ javaScript/objectsBuiltInMethods.js
@@ -0,0 +1,16 @@
+// Objects Built-in methods
+const obj = Object.create(null);
+console.log(obj);
+
+const obj1 = Object.assign({}, obj);
+console.log(obj1);
+
+const obj2 = Object.freeze();
+
+const obj3 = Object.keys();
+
+const obj4 = Object.values();
+
+const obj5 = Object.entries();
+
+const obj6 = Object.fromEntries();
